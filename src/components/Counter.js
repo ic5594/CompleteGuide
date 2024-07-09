6a80246752da8f0bba662ece70bcbe362391e50d
ic5594 <ic5594@naver.com>
2024-07-09 10:07:39 +0900

    redux toolkit 설정

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,37 @@
 import classes from './Counter.module.css';
+import {useSelector, useDispatch} from 'react-redux'
+import { counterActions } from '../store/index'
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+  const show =  useSelector(state => state.showCounter)
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(10))
+  }
+
+  const increamentHandler = () => {
+    dispatch(counterActions.increment())
+  };
+
+  const decreamentHandler = () => {
+    dispatch(counterActions.decrement())
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={increamentHandler}>increament</button>
+        <button onClick={increaseHandler}>increamentby5</button>
+        <button onClick={decreamentHandler}>decreament</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
